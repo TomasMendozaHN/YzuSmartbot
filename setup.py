5c7b172ef8c8bb22a1c1f046d0f010c379743f66
TomasMendozaHN <tmendozarias@gmail.com>
2021-02-02 14:47:10 +0800

    updated setup.py and created a test method

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='YzuSmartbot',
+    version='0.0.1',
+    description='Reinforcement Learning Environment created for the Jetbot',
+    url='git@github.com:TomasMendozaHN/YzuSmartbot-sdk-package.git',
+    author='Tomas Mendoza',
+    author_email='tmendozarias@gmail.com',
+    license='unlicense',
+    packages=['YzuSmartbot'],
+    zip_safe=False
+)
